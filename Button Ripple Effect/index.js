2513bd9526f240189117a480cbda7169be4e72a8
Maimoona Javed <123667640+maimoona-j@users.noreply.github.com>
2023-04-30 23:03:19 +0500

    Delete Button Ripple Effect directory

--- Button Ripple Effect/index.js
+++
@@ -1,9 +0,0 @@
-const btnEl = document.querySelector(".btn");
-
-btnEl.addEventListener("mouseover", (event) => {
-  const x = event.pageX - btnEl.offsetLeft;
-  const y = event.pageY - btnEl.offsetTop;
-
-  btnEl.style.setProperty("--xPos", x + "px");
-  btnEl.style.setProperty("--yPos", y + "px");
-});
