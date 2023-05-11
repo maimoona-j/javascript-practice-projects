19f72dddb0900ee0890055260ef789897df8ad5d
Maimoona Javed <123667640+maimoona-j@users.noreply.github.com>
2023-05-11 23:09:35 +0500

    Javascript-mini-projects
    Javascript-mini-projects

---
+++ Testimonial slider/index.js
@@ -0,0 +1,42 @@
+const testimonials = [
+  {
+    name: "Cherista G",
+    photoUrl:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    text: "I could probably go into sales for you. Smart watch is both attractive and highly adaptable. Wow what great service, I love it! I like smart watch more and more each day because it makes my life a lot easier",
+  },
+  {
+    name: "Rosetta Q",
+    photoUrl:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+    text: "I will recommend you to my colleagues. I could probably go into sales for you. Thanks guys, keep up the good work!",
+  },
+  {
+    name: "Constantine V",
+    photoUrl:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    text: "Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. The very best.",
+  },
+];
+
+const imgEl = document.querySelector("img");
+const textEl = document.querySelector(".text");
+const usernameEl = document.querySelector(".username");
+
+let idx = 0;
+
+updateTestimonial();
+
+function updateTestimonial() {
+  const { name, photoUrl, text } = testimonials[idx];
+  imgEl.src = photoUrl;
+  textEl.innerText = text;
+  usernameEl.innerText = name;
+  idx++;
+  if (idx === testimonials.length) {
+    idx = 0;
+  }
+  setTimeout(() => {
+    updateTestimonial();
+  }, 2000);
+}
